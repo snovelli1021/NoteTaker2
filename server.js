aac2d36c148a38e6f9ca8a5a810305ab29d89981
Stephen Novelli <snovelli1021@gmail.com>
2022-09-22 18:39:29 -0700

    Setup api GET route to disply notes in the browser.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
+const fs = require("fs");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -12,11 +13,23 @@ app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
 
-//These are the GET Routes that render the 2 html pages when the server is running.
+//These are the GET routes that render the 2 html pages when the server is running.
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+//This api GET route returns the stored notes from the db.json file on start up.
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", function (err, data) {
+    var returnNotes = JSON.parse(data);
+    res.json(returnNotes);
+  });
+});
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
